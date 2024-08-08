bb74d3fb4513197035e13764868d2dee07f5a742
Bettino Gaussaint <120162824+BettinoCodes@users.noreply.github.com>
2024-08-07 20:25:25 -0400

    Update main.py

--- Day54-55:FlaskHTMLURLParsing/main.py
+++ Day54-55:FlaskHTMLURLParsing/main.py
@@ -1,8 +1,16 @@
-#learning some flask
-
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/username/<name>/<int:number>')
+def hello_name(name, number):
+    return f'<h1>Hello {name}, your are {number} years old!</h1>'\
+            '<p>This is a paragraph</p>'\
+            '<img src="https://www.wsupercars.com/wallpapers-regular/BMW/2024-BMW-XM-Label-Red-001-1080.jpg">'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
