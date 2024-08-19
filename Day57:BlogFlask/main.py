980f9da9afc074cb018471fde7529018fc83152c
Bettino Gaussaint <120162824+BettinoCodes@users.noreply.github.com>
2024-08-18 21:41:54 -0400

    Update main.py

--- Day57:BlogFlask/main.py
+++ Day57:BlogFlask/main.py
@@ -8,10 +8,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def start_screen():
-    return "Hello User Welcome to the Site"
+    year_now = datetime.datetime.now().year
+    return  render_template("index.html", current_year=year_now)
 
 @app.route("/guess/<vname>")
 def guess_name(vname):
+    year_now = datetime.datetime.now().year
     gender_url = f"https://api.genderize.io?name={vname}"
     age_url = f"https://api.agify.io?name={vname}"
     result = requests.get(gender_url)
@@ -20,14 +22,15 @@ def guess_name(vname):
     result_info = result.json()
     p_age = r_age["age"]
     p_gender = result_info["gender"]
-    return render_template("index.html",person_name = vname, age=p_age, gender=p_gender)
+    return render_template("index.html",person_name = vname, age=p_age, gender=p_gender, current_year=year_now)
 
 @app.route("/blogs")
 def blogs_posted():
+    year_now = datetime.datetime.now().year
     res = requests.get("https://api.npoint.io/685c852ac3a06729360e")
     post = res.json()
     all_post = post["blogs"]
-    return render_template("blogs.html", posts=all_post)
+    return render_template("blogs.html", posts=all_post, current_year=year_now)
 
 if __name__ == "__main__":
     app.run(debug=True)
